c3110d0f25db7b2bcf3b767181ed70d4be2396a6
platzhersh <chregi.glatthard@gmail.com>
2016-09-09 23:47:25 +0200

    fix static config

--- websockets/settings.py
+++ websockets/settings.py
@@ -117,9 +117,16 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 
 CHANNEL_LAYERS = {
